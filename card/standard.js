589311076ef6e3ff9a69c962653943745b2f4b49
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2020-09-15 20:04:02 +0800

    fix xiuluolianyuji weapon and baiyin armor situation.

--- card/standard.js
+++ card/standard.js
@@ -168,6 +168,9 @@ game.import('card',function(lib,game,ui,get,ai,_status){
 								}
 								return -0.5;
 							}
+							var xiuluolianyuji_weapon=player.getEquip(1)&&player.getEquip(1).name=='xiuluolianyuji';
+							var baiyin_armor=target.getEquip(2)&&target.getEquip(2).name=='baiyin';
+							if(xiuluolianyuji_weapon&&baiyin_armor) return 1;
 							return -1.5;
 						},
 					},
