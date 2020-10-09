7e524ee806a12b0268faa1fb2c7859358632764e
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2020-10-09 14:10:17 +0800

    Remove connect mode check of player number and extension.

--- game/game.js
+++ game/game.js
@@ -7217,16 +7217,14 @@
 								}
 								_status.evaluatingExtension=false;
 							}
-							else if(lib.config.mode!='connect'||show_splash){
+							else {
 								extensionlist.push(lib.config.extensions[i]);
 							}
 						}
 					}
 					else{
-						if(lib.config.mode!='connect'||show_splash){
-							for(var i=0;i<lib.config.extensions.length;i++){
-								game.import('extension',{name:lib.config.extensions[i]});
-							}
+						for(var i=0;i<lib.config.extensions.length;i++){
+							game.import('extension',{name:lib.config.extensions[i]});
 						}
 					}
 					var loadPack=function(){
@@ -7289,7 +7287,7 @@
 					var styleLoaded=function(){
 						styleToLoad--;
 						if(styleToLoad==0){
-							if(extensionlist.length&&(lib.config.mode!='connect'||show_splash)){
+							if(extensionlist.length){
 								var extToLoad=extensionlist.length;
 								var extLoaded=function(){
 									extToLoad--;
@@ -43069,10 +43067,12 @@
 						for(var i of game.connectPlayers){
 							if(!i.nickname&&!i.classList.contains('unselectable2')) num++;
 						}
+						/*
 						if(num>=lib.configOL.number-1){
 							alert('至少要有两名玩家才能开始游戏！');
 							return;
 						}
+						*/
 						game.resume();
 					}
 					button.delete();
