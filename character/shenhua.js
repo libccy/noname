bef480bde4a8638945f4c4b34f411cef6ddee1a9
mengxinzxz <139629761+mengxinzxz@users.noreply.github.com>
2023-08-30 21:57:54 +0800

    shenhua.js回滚修复

--- character/shenhua.js
+++ character/shenhua.js
@@ -7023,7 +7023,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 						return true;
 					}
 					else if(event.name=='gain'){
-						if(event.giver||event.getParent().name=='_yongjian_zengyu') return false;
+						if(event.giver||event.getParent().name=='gift') return false;
 						var cards=event.getg(event.player);
 						if(!cards.length) return false;
 						return game.hasPlayer(function(current){
