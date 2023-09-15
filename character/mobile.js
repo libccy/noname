704bd3d65b853436c41a7e0691b9bde68d07ca0c
mengxinzxz <139629761+mengxinzxz@users.noreply.github.com>
2023-09-15 15:59:32 +0800

    bugfix

--- character/mobile.js
+++ character/mobile.js
@@ -405,7 +405,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 				direct:true,
 				content:function(){
 					'step 0'
-					player.chooseTarget(get.prompt('meihun'),'选择一名其他角色并选择一个花色，获得其此花色的所有牌并令其获得“雀”标记',lib.filter.notMe).set('ai',function(target){
+					player.chooseTarget(get.prompt('yichong'),'选择一名其他角色并选择一个花色，获得其此花色的所有牌并令其获得“雀”标记',lib.filter.notMe).set('ai',function(target){
 						var player=_status.event.player;
 						var att=get.attitude(player,target);
 						if(att>0) return 0;
