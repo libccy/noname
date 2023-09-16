76f780041ce0c55efac5ea9416899bc541c4544c
mengxinzxz <139629761+mengxinzxz@users.noreply.github.com>
2023-09-17 00:17:29 +0800

    许攸成略添加放置卡牌prompt

--- character/shenhua.js
+++ character/shenhua.js
@@ -2174,6 +2174,9 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 					}
 					return true;
 				},
+				prompt:function(event,player){
+					return get.prompt('nzry_shicai')+'（将'+get.translation(event.cards.filterInD('oe'))+'置于牌堆顶）';
+				},
 				content:function(){
 					'step 0'
 					event.cards=trigger.cards.filterInD('oe');
