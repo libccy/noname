8c0695b0ed2c6c7e335f9ca129c28a54208a1e3a
lieren2023 <131325076+lieren2023@users.noreply.github.com>
2023-09-20 16:25:31 +0800

    懒加载修复（回滚）

--- game/game.js
+++ game/game.js
@@ -41425,7 +41425,7 @@
 						var page=ui.create.div('');
 						var node=ui.create.div('.menubutton.large',info.name,position,clickMode);
 						node.mode=mode;
-						node._initLink=function(){
+						// node._initLink=function(){
 							node.link=page;
 							var map={};
 							if(info.config){
@@ -41812,8 +41812,8 @@
 									info.config.update(config,map);
 								}
 							}
-						};
-						if(!get.config('menu_loadondemand')) node._initLink();
+						// };
+						// if(!get.config('menu_loadondemand')) node._initLink();
 						return node;
 					};
 
