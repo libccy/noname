14cec5b6df0f197ca15d4d4934417cb69d3219fe
mengxinzxz <139629761+mengxinzxz@users.noreply.github.com>
2023-09-26 13:42:43 +0800

    Update sp.js

--- character/sp.js
+++ character/sp.js
@@ -1237,9 +1237,9 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 							return player.inRange(current)&&get.effect(current,{name:'sha'},player,player)>0;
 						})) return 0;
 						var rand=Math.random();
-						if(rand<0.2&&list.includes(0)) return 0;
-						if(rand<0.7&&list.includes(1)) return 1;
-						if(rand<1.0&&list.includes(2)) return 2;
+						if(rand<0.2) return 0;
+						if(rand<0.7) return 1;
+						if(rand<1.0) return 2;
 						return get.rand(0,2);
 					}());
 					'step 1'
