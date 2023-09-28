03731d2844e0cc3854119e0b4251598867a1640f
Spmario233 <Spmario233@163.com>
2023-09-28 22:01:35 +0800

    Update update.js

--- game/update.js
+++ game/update.js
@@ -4,6 +4,7 @@ window.noname_update={
 	changeLog:[
 		'整合@rintim @mengxinzxz @lieren2023 @PZ157 @Tipx-L @kuangshen04 @nonameShijian @copcap的Pull Request',
 		'新武将：界钟繇、武陆逊、界文聘、许靖、孟优、陈式、费曜、孙礼、夏侯楙、OL陆郁生、☆周不疑、',
+		'音效优化：增加不同属性伤害的不同音效',
 		'技能优化：神典韦、手杀南华老仙等',
 		'机制更新: 属性【杀】机制完善优化等',
 		'界面更新: 优化自带代码编辑器；启动页增加新样式；武将资料页美化等',
