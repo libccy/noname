5778c052f4a98d7ac7747eb4ad62cb8fc325f5df
lieren2023 <131325076+lieren2023@users.noreply.github.com>
2023-10-30 23:09:20 +0800

    调整合并坐骑栏选项至显示菜单

--- game/game.js
+++ game/game.js
@@ -735,12 +735,6 @@
 			general:{
 				name:'通用',
 				config:{
-					mount_combine:{
-						name:'合并坐骑栏',
-						init:false,
-						intro:'<li>将进攻坐骑栏和防御坐骑栏合并为同一个位置（重启后生效）。',
-						restart:true,
-					},
 					low_performance:{
 						name:'流畅模式',
 						init:false,
@@ -3649,6 +3643,12 @@
 						else{
 							map.show_extensionshare.hide();
 						}
+					},
+					mount_combine:{
+						name:'合并坐骑栏',
+						init:false,
+						intro:'<li>将进攻坐骑栏和防御坐骑栏合并为同一个位置（重启后生效）。',
+						restart:true,
 					},
 					show_history:{
 						name:'出牌记录栏',
