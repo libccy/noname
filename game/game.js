cb8d95c61731e1a8cd240f21da447a55e9bb29d7
shijian <2954700422@qq.com>
2023-12-27 23:04:22 +0800

    noname.js统一导出

--- game/game.js
+++ game/game.js
@@ -50,9 +50,6 @@ new Promise(resolve => {
 	}
 	window['b' + 'ann' + 'e' + 'dE' + 'x' + 'ten' + 's' + 'i' + 'o' + 'ns'] = ['\u4fa0\u4e49', '\u5168\u6559\u7a0b'];
 
-	/**
-	 * @type { Promise<import('../noname.js')> }
-	 */
 	const module = import('../noname.js');
 
 	module.then(({ ai, game, get, lib, _status, ui, boot }) => {
--- noname.js
+++ noname.js
@@ -1,19 +1,8 @@
-import { GNC as gnc } from './noname/gnc/index.js';
-import { AI as ai } from './noname/ai/index.js';
-import { Game as game } from './noname/game/index.js';
-import { Get as get } from './noname/get/index.js';
-import { Library as lib } from './noname/library/index.js';
-import { status as _status } from './noname/status/index.js';
-import { UI as ui } from './noname/ui/index.js';
-
+export { GNC as gnc } from './noname/gnc/index.js';
+export { AI as ai } from './noname/ai/index.js';
+export { Game as game } from './noname/game/index.js';
+export { Get as get } from './noname/get/index.js';
+export { Library as lib } from './noname/library/index.js';
+export { status as _status } from './noname/status/index.js';
+export { UI as ui } from './noname/ui/index.js';
 export { boot } from './noname/init/index.js';
-
-export {
-	gnc,
-	ai,
-	game,
-	get,
-	lib,
-	_status,
-	ui,
-}
