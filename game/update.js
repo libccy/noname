59b54190c5471222369b213cad23cb1b3da3c3d5
Spmario233 <Spmario233@163.com>
2024-01-28 22:43:31 +0800

    v1.10.6.3 metadata

--- game/update.js
+++ game/update.js
@@ -1,76 +1,57 @@
 window.noname_update={
-	version:'1.10.6.2',
-	update:'1.10.6.1',
+	version:'1.10.6.2.1',
+	update:'1.10.6.2',
 	changeLog:[
-		'整合@Curpond @PZ157 @copcap @nonameShijian @Tipx-L @universe-st @kuangshen04 @mengxinzxz @Rintim @tangXins 的Pull Request',
-		'OL李异；新服群孟获、凌操、清河公主；手杀神鲁肃、霍峻、木鹿大王、谋曹丕；',
+		'整合@PZ157 @nonameShijian @universe-st @mengxinzxz 的Pull Request',
+		'OL族吴乔、谋关羽；海外服侠关羽',
 		'其他AI优化与bug修复',
 	],
 	files:[
-		'card/extra.js',
-		'card/guozhan.js',
 		'card/standard.js',
-		'card/yongjian.js',
 
 		'character/clan.js',
-		'character/collab.js',
-		'character/ddd.js',
 		'character/diy.js',
 		'character/extra.js',
 		'character/gujian.js',
 		'character/gwent.js',
 		'character/hearth.js',
 		'character/huicui.js',
-		'character/jiange.js',
 		'character/jsrg.js',
 		'character/mobile.js',
-		'character/mtg.js',
 		'character/offline.js',
-		'character/old.js',
 		'character/onlyOL.js',
-		'character/ow.js',
 		'character/rank.js',
 		'character/refresh.js',
 		'character/sb.js',
 		'character/shenhua.js',
-		'character/shiji.js',
 		'character/sp.js',
 		'character/sp2.js',
 		'character/standard.js',
-		'character/swd.js',
 		'character/tw.js',
-		'character/xiake.js',
 		'character/xianding.js',
-		'character/xianjian.js',
-		'character/xinghuoliaoyuan.js',
 		'character/yijiang.js',
-		'character/yingbian.js',
-		'character/yxs.js',
-		'character/zhuogui.js',
-
-		'game/entry.js',
-		'game/game.js',
-		'game/keyWords.js',
 
 		'mode/guozhan.js',
+
+		'noname/ai/basic.js',
 
 		'noname/game/index.js',
 
 		'noname/get/index.js',
 
-		'noname/init/cordova.js',
-		'noname/init/import.js',
-		'noname/init/index.js',
-
 		'noname/library/index.js',
+
+		'noname/library/cache/cacheContext.js',
+		'noname/library/cache/childNodesWatcher.js',
+		
+		'noname/library/crypt/md5.js',
 
 		'noname/library/element/card.js',
 		'noname/library/element/content.js',
 		'noname/library/element/gameEventPromise.js',
 		'noname/library/element/player.js',
+		'noname/library/element/vcard.js',
 
-		'noname/util/index.js',
-
-		'noname/util/struct/promise-error-handler/chrome.js',
+		'noname/ui/index.js',
 	]
 };
