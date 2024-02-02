e36c497ef6669df3ca3f4905b7c5f345ab285ee2
mengxinzxz <2223529500@qq.com>
2024-02-02 11:49:19 +0800

    bugfix

--- character/huicui.js
+++ character/huicui.js
@@ -180,11 +180,11 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 						mod:{
 							targetEnabled(card,player,target){
 								const list=lib.skill.dczixi.zixiList;
-								if(!list.includes(card.name)&&!list.includes('dczixi_'+card.name)) return;
 								if(card.name.indexOf('dczixi_')==0){
-									if(target.hasJudge(card.name.slice('dczixi_'.length))) return false;
+									const name=card.name.slice('dczixi_'.length);
+									if(list.includes(name)&&target.hasJudge(name)) return false;
 								}
-								else if(target.hasJudge('dczixi_'+card.name)) return false;
+								else if(list.includes(card.name)&&target.hasJudge('dczixi_'+card.name)) return false;
 							},
 						},
 						ai:{
