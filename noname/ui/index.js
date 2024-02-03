2e510ba5a3868765d2a9ac5e015932f07fbef096
lieren2023 <131325076+lieren2023@users.noreply.github.com>
2024-02-03 17:29:01 +0800

    未分类武将自动添加至等待分包（可开关）

--- noname/ui/index.js
+++ noname/ui/index.js
@@ -2862,11 +2862,39 @@ class Create extends Uninstantable {
 							}
 						}
 						if (list2.length) {
-							var cfgnodeX = createConfig({
-								name: '其他',
-								_name: 'others',
-								clear: true,
-							});
+							var cfgnodeY={
+								name:'等待分包',
+								_name:'others',
+								init:true,
+								onclick:function(bool){
+									var banned=[];
+									if(connectMenu){
+										var modex=menux.pages[0].firstChild.querySelector('.active');
+										if(modex&&modex.mode){
+											banned=lib.config['connect_'+modex.mode+'_banned'];
+										}
+									}
+									else if(_status.connectMode) return;
+									else banned=lib.config[get.mode()+'_banned']||[];
+									if(bool){
+										for(var i=0;i<list2.length;i++){
+											banned.remove(list2[i]);
+										}
+									}
+									else{
+										for(var i=0;i<list2.length;i++){
+											banned.add(list2[i]);
+										}
+									}
+									game.saveConfig(connectMenu?('connect_'+modex.mode+'_banned'):(get.mode()+'_banned'),banned);
+									updateActive();
+								},
+							};
+							if(mode.startsWith('mode_')&&!mode.startsWith('mode_extension_')&&!mode.startsWith('mode_guozhan')){
+								cfgnodeY.clear=true;
+								delete cfgnodeY.onclick;
+							}
+							var cfgnodeX=createConfig(cfgnodeY);
 							page.appendChild(cfgnodeX);
 							var buttons = ui.create.buttons(list2, 'character', page);
 							for (var i = 0; i < buttons.length; i++) {
