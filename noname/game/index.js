e0f5b9ee20f3bde4e74c16e61b8f8c31270bca3f
Spmario233 <Spmario233@163.com>
2024-02-17 20:35:36 +0800

    缩进修复

--- noname/game/index.js
+++ noname/game/index.js
@@ -1889,7 +1889,7 @@ export class Game extends Uninstantable {
     			}
 			}catch(e1){
 				console.log(`加载《${name}》扩展的precontent时出现错误。`,e1);
-			    if(!lib.config.extension_alert) alert(`加载《${name}》扩展的precontent时出现错误。\n该错误本身可能并不影响扩展运行。您可以在“设置→通用→无视扩展报错”中关闭此弹窗。\n${decodeURI(e1.stack)}`);
+				if(!lib.config.extension_alert) alert(`加载《${name}》扩展的precontent时出现错误。\n该错误本身可能并不影响扩展运行。您可以在“设置→通用→无视扩展报错”中关闭此弹窗。\n${decodeURI(e1.stack)}`);
 			}
 			
 			if (content) lib.extensions.push([name, content, config, _status.evaluatingExtension, objectPackage || {}]);
