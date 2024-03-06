2348320cb1d5319f60feb53b90da9a99aac60708
nonameShijian <2954700422@qq.com>
2024-03-06 17:37:09 +0800

    添加import *.mold文件的支持，添加ref函数进行最基础的数据绑定的功能，修复index.html缺少一个函数的bug

---
+++ noname/ref/index.js
@@ -0,0 +1,283 @@
+/**
+ * ```js
+ * // 使用例子: 
+ * const btn = xxx;
+ * const count = ref(0);
+ * btn.onclick = () => count.value++;
+ * btn.innerHTML = count.value + 1;
+ * // 手动实现数据绑定
+ * // 但在.mold文件使用无需如此
+ * count.use(btn, { props: [{ 
+ * 	// 属性名
+ * 	key: 'innerHTML',
+ * 	// 更新时执行此字符串决定对应属性的结果
+ * 	value: '{{count.value + 1}}',
+ * 	// 暴露给ref类的执行代码的函数
+ *  evaluateJavascript: str => eval(str),
+ * }], attrs: [] });
+ * 
+ * ```
+ * @template T
+ * @param { T } value
+ * @returns { RefImpl<T> }
+ */
+export function ref(value) {
+	if (value instanceof RefImpl) {
+		return value;
+	}
+	// @ts-ignore
+	if ([undefined, null].includes(value)) throw TypeError('Cannot create proxy with a non-object as ref');
+	return new RefImpl(Object(value));
+}
+
+/**
+ * @template T
+ */
+export class RefImpl {
+	static recordExecution = false;
+	static executionList = [];
+	[Symbol.toPrimitive]() {
+		if (RefImpl.recordExecution === true) {
+			RefImpl.executionList.push(this);
+		}
+		if (this.#value instanceof Number) {
+			return Number(this.#value);
+		}
+		return String(this.#value);
+	}
+	/** @type { T } */
+	#value;
+	/** @type { ({ element: HTMLElement, props: ({ key: string | Symbol, value: string, evaluateJavascript: (str: string) => any })[], attrs: ({ key: string, value: string, evaluateJavascript: (str: string) => any })[] })[] }  */
+	#boundDomList = [];
+	/**
+	 * @param { T } value 
+	 */
+	constructor(value) {
+		this.#value = this.createProxy(value);
+	}
+	get value() {
+		return this.#value;
+	}
+	set value(newVal) {
+		// 更新视图
+		if (this.#value != newVal) {
+			this.#value = this.createProxy(newVal);
+			this.updateDom();
+		}
+	}
+	createProxy(value) {
+		/**
+		 * @type { ProxyHandler<any> }
+		 */
+		const handler = {
+			get(target, prop) {
+				let value = Reflect.get(target, prop);
+				if (typeof value == 'function') {
+					value = value.bind(target);
+				}
+				// console.log('get', target, prop, value);
+				if (Object(value) !== value && Boolean(value)) {
+					return new Proxy(value, handler);
+				}
+				return value;
+			},
+			set: (target, prop, newVal, receiver) => {
+				const oldVal = Reflect.get(target, prop);
+				// console.log('set', target, prop, oldVal, newVal);
+				const result = Reflect.set(target, prop, newVal, receiver);
+				// 更新视图
+				if (oldVal != newVal) {
+					this.updateDom();
+				}
+				return result;
+			}
+		};
+		if ([undefined, null].includes(value)) throw TypeError(' Cannot create proxy with a non-object as ref');
+		return new Proxy(Object(value), handler);
+	}
+	/**
+	 * @param { HTMLElement } element
+	 * @param { Object } param1 
+	 * @param { ({ key: string | Symbol, value: string, evaluateJavascript: (str: string) => any })[] } [param1.props] 
+	 * @param { ({ key: string, value: string, evaluateJavascript: (str: string) => any })[] } [param1.attrs] 
+	 */
+	use(element, { props, attrs }) {
+		const bound = this.#boundDomList.find(v => v.element == element) || { element, props: [], attrs: [] };
+		if (!this.#boundDomList.includes(bound)) {
+			this.#boundDomList.push(bound);
+		}
+		if (Array.isArray(props)) bound.props = Array.from(new Set(bound.props.concat(props)));
+		if (Array.isArray(attrs)) bound.attrs = Array.from(new Set(bound.attrs.concat(attrs)));
+	}
+	updateDom() {
+		this.#boundDomList.forEach(({ element, props, attrs }) => {
+			props.forEach(({ key, value, evaluateJavascript }) => {
+				// @ts-ignore
+				if (!['innerText', 'innerHTML'].includes(key)) {
+					// @ts-ignore
+					element[key] = evaluateJavascript(value);
+				} else {
+					// @ts-ignore
+					element[key] = value.replaceAll(/\{\{(.+?)\}\}/g, (_, str) => {
+						try {
+							return evaluateJavascript(str.trim()) ?? `{{${str}}}`;
+						} catch {
+							return `{{${str}}}`;
+						}
+					});
+				}
+			});
+			attrs.forEach(({ key, value, evaluateJavascript }) => element.setAttribute(key, evaluateJavascript(value)));
+		});
+	}
+}
+
+window.mold = {
+	templateMap: new Map(),
+	registerEvaluate(url, evaluateJavascript) {
+		let obj = this.templateMap.get(url);
+		if (!obj) {
+			this.templateMap.set(url, {
+				evaluateJavascript
+			});
+		} else {
+			// @ts-ignore
+			obj.evaluateJavascript = evaluateJavascript;
+		}
+		// @ts-ignore
+		compileDom(this.templateMap.get(url).template, evaluateJavascript);
+	},
+};
+
+/**
+ * @param { HTMLTemplateElement | HTMLElement } el
+ * @param { (str: string) => any } evaluateJavascript 
+ */
+export function compileDom(el, evaluateJavascript) {
+	if (el instanceof HTMLTemplateElement) {
+		const { content } = el;
+		if (content.children) {
+			// @ts-ignore
+			[...content.children].forEach(ele => compileDom(ele, evaluateJavascript));
+		}
+		// @ts-ignore
+		el.parentElement.appendChild(content);
+		// @ts-ignore
+		el.parentElement.removeChild(el);
+	} else {
+		const attrNames = el.getAttributeNames();
+
+		// 处理@xx的事件
+		const names = attrNames.filter(n => n.startsWith('@'));
+		if (names.length > 0) {
+			names.forEach(evtName => {
+				const funVal = el.getAttribute(evtName);
+				if (funVal != null) {
+					try {
+						if (funVal.indexOf('(') == -1 || funVal.indexOf(')') == -1) {
+							let fun = evaluateJavascript(funVal);
+							if (typeof fun == 'function') {
+								el.addEventListener(evtName.slice(1), (event) => {
+									if (fun.length < 2) {
+										fun.call(el, event);
+									} else {
+										fun.call(el);
+									}
+								});
+							}
+						} else {
+							throw new Error('带小括号的不执行');
+						}
+					} catch (e) {
+						try {
+							let fun = evaluateJavascript(`(() => function ${evtName.slice(1)} (event) {\n\t${funVal}\n})()`);
+							el.addEventListener(evtName.slice(1), event => {
+								fun.call(el, event);
+							});
+						} catch (ex) {
+							console.error(`[${evtName}] ${ex}`);
+						}
+					}
+				}
+				el.removeAttribute(evtName);
+			});
+		}
+
+		// 处理:xx的标签属性
+		const evaluateNames = attrNames.filter(n => n.startsWith(':'));
+		if (evaluateNames.length > 0) {
+			evaluateNames.forEach(name => {
+				const val = el.getAttribute(name);
+				RefImpl.recordExecution = true;
+				RefImpl.executionList.length = 0;
+				// @ts-ignore
+				const getVal = evaluateJavascript(val);
+				if (!(getVal instanceof RefImpl)) {
+					if (RefImpl.executionList.length > 0) {
+						RefImpl.executionList.forEach(ref => {
+							ref.use(el, {
+								props: [{
+									key: name.slice(1),
+									// @ts-ignore
+									value: val,
+									evaluateJavascript,
+								}],
+							});
+						});
+					}
+					el.setAttribute(name.slice(1), getVal);
+				} else {
+					el.setAttribute(name.slice(1), evaluateJavascript(getVal.value));
+					getVal.use(el, {
+						attrs: [{
+							key: name.slice(1),
+							// @ts-ignore
+							value: val,
+							evaluateJavascript,
+						}],
+					});
+				}
+				el.removeAttribute(name);
+				RefImpl.recordExecution = false;
+				RefImpl.executionList.length = 0;
+			});
+		}
+
+		if (el.innerHTML.length > 0) {
+			const oldVal = el.innerHTML;
+			el.innerHTML = oldVal.replace(/\{\{(.+?)\}\}/g, (_, str) => {
+				RefImpl.recordExecution = true;
+				RefImpl.executionList.length = 0;
+				const getVal = evaluateJavascript(str);
+				if (getVal instanceof RefImpl) {
+					getVal.use(el, {
+						props: [{
+							key: 'innerHTML',
+							// @ts-ignore
+							value: oldVal,
+							evaluateJavascript,
+						}],
+					});
+				} else if (RefImpl.executionList.length > 0) {
+					RefImpl.executionList.forEach(ref => {
+						ref.use(el, {
+							props: [{
+								key: 'innerHTML',
+								// @ts-ignore
+								value: oldVal,
+								evaluateJavascript,
+							}],
+						});
+					});
+				}
+				RefImpl.recordExecution = false;
+				RefImpl.executionList.length = 0;
+				return getVal;
+			});
+		}
+		if (el.children) {
+			// @ts-ignore
+			[...el.children].forEach(ele => compileDom(ele, evaluateJavascript));
+		}
+	}
+};
--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ self.addEventListener('fetch', event => {
 	// @ts-ignore
 	const request = event.request;
 	if (typeof request.url != 'string') return console.log(request);
-	if (!['.ts', '.json'].some(ext => request.url.endsWith(ext))) return;
+	if (!['.ts', '.json', 'mold'].some(ext => request.url.endsWith(ext))) return;
 	if (request.url.endsWith('.d.ts')) return;
 	if (request.url.endsWith('.json')) {
 		// @ts-ignore
@@ -55,7 +55,7 @@ self.addEventListener('fetch', event => {
 				let js;
 				if (request.url.endsWith('.json')) {
 					js = `export default ${text}`;
-				} else {
+				} else if (request.url.endsWith('.ts')) {
 					js = ts.transpile(text, {
 						module: ts.ModuleKind.ES2015,
 						target: ts.ScriptTarget.ES2019,
@@ -63,6 +63,67 @@ self.addEventListener('fetch', event => {
 						resolveJsonModule: true,
 						esModuleInterop: true,
 					}, request.url);
+				} else if (request.url.endsWith('.mold')) {
+					// 使用例子: import('./index.mold').then(({ default: { render } }) => {
+					// render(ui.window)
+					// })
+					js = `
+						// mold字符串
+						const text = ${JSON.stringify(text)};
+						// mold文件绝对路径（因为和这个js同目录所以这么写）
+						const url = '${request.url}';
+
+						// 字符串转dom
+						const doc = document.implementation.createHTMLDocument('${request.url.slice(request.url.lastIndexOf('/') + 1)}');
+						doc.body.innerHTML = '<base href="' + url + '">' + text;
+
+						// 获取dom的template，script，style标签
+						const templates = [...doc.querySelectorAll('template')];
+						const scripts = [...doc.querySelectorAll('script')];
+						const styles = [...doc.querySelectorAll('style')];
+
+						const render = (parent, ...props) => {
+							// 暂时每种标签只解析第一个
+							const template = templates[0].cloneNode(true);
+							if (!window.mold.templateMap.get(url)) {
+								window.mold.templateMap.set(url, {
+									template
+								});
+							} else {
+								window.mold.templateMap.get(url).template = template;
+							}
+							const script = document.createElement('script');
+							script.setAttribute('type', 'module');
+							let scriptText = '';
+							const insertEnd = '\\nwindow.mold.registerEvaluate(import.meta.url, str => eval(str));';
+							if (scripts[0]) {
+								if (typeof ts != 'undefined' && scripts[0].setAttribute('lang') === 'ts') {
+									scriptText = ts.transpile(scripts[0].innerText + insertEnd, {
+										module: ts.ModuleKind.ES2015,
+										target: ts.ScriptTarget.ES2019,
+										inlineSourceMap: true,
+										resolveJsonModule: true,
+										esModuleInterop: true,
+									}, url);
+								} else {
+									scriptText = scripts[0].cloneNode(true).innerText + insertEnd;
+								}
+							}
+							script.innerHTML = scriptText;
+
+							const style = styles[0].cloneNode(true);
+							
+							const tmpBaseElt = document.createElement('base');
+							tmpBaseElt.href = url;
+							document.head.appendChild(tmpBaseElt);
+							document.head.appendChild(style);
+							document.head.appendChild(script);
+							parent.appendChild(template);
+							document.head.removeChild(tmpBaseElt);
+						}
+						export default { 
+							render
+						}`.replaceAll(`						`, '');
 				}
 				const rep = new Response(new Blob([js], { type: "text/javascript" }), {
 					status: 200,
