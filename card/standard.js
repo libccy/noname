bf5eebca09036246f084a5a34719fd59a97d2da4
Spmario233 <Spmario233@163.com>
2024-03-30 20:19:51 +0800

    无懈bug修复，boss名称占位修复

--- card/standard.js
+++ card/standard.js
@@ -2985,6 +2985,7 @@ game.import('card',function(lib,game,ui,get,ai,_status){
 					return true;
 				},
 				forceLoad:true,
+				forceDie:true,
 				content:function(){
 					'step 0'
 					delete event.wuxieresult;
