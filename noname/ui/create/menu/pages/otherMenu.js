d2f1941aa4f48175a76cfad076f745eaa347eb06
mengxinzxz <2223529500@qq.com>
2024-04-17 12:12:48 +0800

    更新内容弹出bugfix

--- noname/ui/create/menu/pages/otherMenu.js
+++ noname/ui/create/menu/pages/otherMenu.js
@@ -349,7 +349,7 @@ export const otherMenu = function (/** @type { boolean | undefined } */ connectM
 								}
 							}
 							const str =
-								versionResult > 0
+								versionResult < 0
 									? `有新版本${description.name}可用，是否下载？`
 									: `本地版本${lib.version}高于或等于github版本${description.name}，是否强制下载？`;
 							const str2 = description.body;
