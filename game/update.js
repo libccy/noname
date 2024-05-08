ee5d3a1011d613e20e4c5c5e108397b8048cccc6
Spmario233 <Spmario233@163.com>
2024-05-08 08:56:38 +0800

    v1.10.12 metadata

--- game/update.js
+++ game/update.js
@@ -1,7 +1,10 @@
 window.noname_update = {
-	version: "1.10.11.3",
+	version: "1.10.12",
 	update: "NULL", //新版本更新文件较多，直接强制进行全量更新
 	changeLog: [
+		"吴懿(十周年地主)、族钟繇、关樾、吴普、星孙坚、令狐愚、司马孚、宣公主、徐琨",
+		"给无名杀的所有技能语音添加对应的台词",
+		"创建Character类",
 		"bug修复",
 	],
 	files: [],
