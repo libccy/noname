3274454ab7f063b41a6e068abbb94b3179672626
lieren2023 <131325076+lieren2023@users.noreply.github.com>
2024-05-14 20:02:31 +0800

    minor fix：台词标点-jsrg

--- character/jsrg/voices.js
+++ character/jsrg/voices.js
@@ -1,6 +1,6 @@
 export default {
-	"#jsrg_zoushi:die": "年老色衰了吗……",
+	"#jsrg_zoushi:die": "年老色衰了吗？",
 	"#jsrg_zhangren:die": "老臣，绝不事二主！",
 	"#jsrg_huangzhong:die": "不得不服老啦~",
-	"#jsrg_liuhong:die": "权利的滋味，让人沉沦。",
+	"#jsrg_liuhong:die": "权利的滋味，让人沉沦……",
 };
