3b0379d68f840c30bd7c31ea6daeccff2892f250
IceCola <739201322@qq.com>
2024-05-19 20:48:57 +0800

    补充2024标准包的characterReplace

--- character/xianding/characterReplace.js
+++ character/xianding/characterReplace.js
@@ -1,11 +1,11 @@
 const characterReplaces = {
-	wenyang: ["wenyang", "db_wenyang", "diy_wenyang"],
+	wenyang: ["wenyang", "db_wenyang", "diy_wenyang", "std_db_wenyang"],
 	gexuan: ["gexuan", "tw_gexuan"],
 	panshu: ["panshu", "re_panshu"],
 	yangwan: ["yangwan", "sp_yangwan"],
 	sunyi: ["re_sunyi", "sunyi", "tw_sunyi"],
 	fengfangnv: ["fengfangnv", "re_fengfangnv"],
-	luotong: ["dc_luotong", "luotong"],
+	luotong: ["dc_luotong", "luotong", "std_dc_luotong"],
 	dc_wangchang: ["dc_wangchang", "tw_wangchang"],
 	guozhao: ["guozhao", "xin_guozhao", "jsrg_guozhao"],
 	dingshangwan: ["dingshangwan", "ol_dingshangwan"],
