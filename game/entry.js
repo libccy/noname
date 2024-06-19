879eafb8728fead5c4971de14d75130442c30995
Rintim <rintim@foxmail.com>
2024-06-19 18:25:26 +0800

    pref: maybe here.

--- game/entry.js
+++ game/entry.js
@@ -1,35 +1,34 @@
+import { UpdateReason } from "../noname-compatible.js";
 import { game, get, lib, boot } from "../noname.js";
 import { canUseHttpProtocol, sendUpdate } from "../noname/init/index.js";
-import { userAgent } from "../noname/util/index.js";
 
-const coreAndVersion = get.coreInfo();
-const core = coreAndVersion[0],
-	version = coreAndVersion[1];
-//@todo: 77 -> 80
-if (core === "chrome" && !isNaN(version) && version < 77) {
-	const tip =
-		"检测到您的浏览器内核版本小于77，请及时升级浏览器或手机webview内核！";
+let [core, version] = get.coreInfo();
+
+/**
+ * @type {Promise<unknown>}
+ */
+let waitUpdate = Promise.resolve();
+if (core === "chrome" && !isNaN(version) && version < 85) {
+	/*
+	const tip = "检测到您的浏览器内核版本小于85，请及时升级浏览器或手机webview内核！";
 	console.warn(tip);
 	game.print(tip);
 	const redirect_tip = `您使用的浏览器或无名杀客户端内核版本过低，将在未来的版本被废弃！\n目前使用的浏览器UA信息为：\n${userAgent}\n点击“确认”以前往GitHub下载最新版无名杀客户端（可能需要科学上网）。`;
 	if (confirm(redirect_tip)) {
-		window.open(
-			"https://github.com/libccy/noname/releases/tag/chromium77-client"
-		);
+		window.open("https://github.com/libccy/noname/releases/tag/chromium77-client");
 	}
+	*/
+	waitUpdate = game.tryUpdateClient(UpdateReason.UNDERSUPPORT);
 }
 
-boot().then(() => {
+waitUpdate.then(boot).then(() => {
 	// 判断是否从file协议切换到http/s协议
 	if (canUseHttpProtocol()) {
 		// 保存协议的切换状态
 		const saveProtocol = () => {
 			const url = sendUpdate();
 			if (typeof url == "string") {
-				if (
-					typeof window.require == "function" &&
-					typeof window.process == "object"
-				) {
+				if (typeof window.require == "function" && typeof window.process == "object") {
 					// @ts-ignore
 					const remote = require("@electron/remote");
 					const thisWindow = remote.getCurrentWindow();
@@ -55,13 +54,9 @@ boot().then(() => {
 				navigator.notification.activityStop();
 			}
 			game.promises
-				.writeFile(
-					lib.init.encode(JSON.stringify(data)),
-					"./",
-					"noname.config.txt"
-				)
+				.writeFile(lib.init.encode(JSON.stringify(data)), "./", "noname.config.txt")
 				.then(saveProtocol)
-				.catch((e) => {
+				.catch(e => {
 					console.error("升级失败:", e);
 				});
 		};
@@ -94,8 +89,9 @@ boot().then(() => {
 				}
 				game.promises
 					.readFileAsText("noname.config.txt")
-					.then((data) => {
+					.then(data => {
 						return /** @type {Promise<void>} */ (
+							// eslint-disable-next-line no-async-promise-executor
 							new Promise(async (resolve, reject) => {
 								if (!data) return reject("!data");
 								try {
@@ -104,10 +100,7 @@ boot().then(() => {
 										throw "err";
 									}
 									// @ts-ignore
-									if (
-										lib.db &&
-										(!data.config || !data.data)
-									) {
+									if (lib.db && (!data.config || !data.data)) {
 										throw "err";
 									}
 								} catch (e) {
@@ -127,42 +120,25 @@ boot().then(() => {
 								alert("升级前的配置导入成功, 即将自动重启");
 								// @ts-ignore
 								if (!lib.db) {
-									const noname_inited =
-										localStorage.getItem("noname_inited");
-									const onlineKey = localStorage.getItem(
-										lib.configprefix + "key"
-									);
+									const noname_inited = localStorage.getItem("noname_inited");
+									const onlineKey = localStorage.getItem(lib.configprefix + "key");
 									localStorage.clear();
 									if (noname_inited) {
-										localStorage.setItem(
-											"noname_inited",
-											noname_inited
-										);
+										localStorage.setItem("noname_inited", noname_inited);
 									}
 									if (onlineKey) {
-										localStorage.setItem(
-											lib.configprefix + "key",
-											onlineKey
-										);
+										localStorage.setItem(lib.configprefix + "key", onlineKey);
 									}
 									for (let i in data) {
 										localStorage.setItem(i, data[i]);
 									}
 								} else {
 									for (let i in data.config) {
-										await game.putDB(
-											"config",
-											i,
-											data.config[i]
-										);
+										await game.putDB("config", i, data.config[i]);
 										lib.config[i] = data.config[i];
 									}
 									for (let i in data.data) {
-										await game.putDB(
-											"data",
-											i,
-											data.data[i]
-										);
+										await game.putDB("data", i, data.data[i]);
 									}
 								}
 								lib.init.background();
@@ -177,7 +153,8 @@ boot().then(() => {
 						const url = new URL(location.href);
 						url.searchParams.delete("sendUpdate");
 						location.href = url.toString();
-					}).catch(e => {
+					})
+					.catch(e => {
 						if (navigator.notification) {
 							navigator.notification.activityStop();
 						}
