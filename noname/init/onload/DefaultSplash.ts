610a20bed7092a57e4fc16d367d3e5d264ed847a
Rintim <rintim@foxmail.com>
2024-06-27 00:03:33 +0800

    feat: add preview interface.

--- noname/init/onload/DefaultSplash.ts
+++ noname/init/onload/DefaultSplash.ts
@@ -46,6 +46,17 @@ export class DefaultSplash implements IOnloadSplash {
 		this.app.unmount();
 	}
 
+	preview(node: HTMLDivElement): void {
+		node.className = "button character";
+		node.style.width = "200px";
+		node.style.height = `${(node.offsetWidth * 1080) / 2400}px`;
+		node.style.display = "flex";
+		node.style.flexDirection = "column";
+		node.style.alignItems = "center";
+		node.style.backgroundSize = "100% 100%";
+		node.setBackgroundImage(`image/splash/${this.id}.jpg`);
+	}
+
 	handle(mode: string) {
 		return `${this.path}/${mode}.jpg`;
 	}
--- noname/init/onload/IOnloadSplash.ts
+++ noname/init/onload/IOnloadSplash.ts
@@ -2,7 +2,9 @@ export interface IOnloadSplash {
 	id: string;
 	name: string;
 
-	init(node: HTMLDivElement, resolve: (mode: string) => void): Promise<void>;
+	init(node: HTMLDivElement, resolve: (mode: string) => void): void | Promise<void>;
 
-	dispose(node: HTMLDivElement): Promise<void>;
+	dispose(node: HTMLDivElement): void | Promise<void>;
+
+	preview(node: HTMLDivElement): void | Promise<void>;
 }
