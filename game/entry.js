c4e2f57eed656e009598fda661db6e93adc4e671
Rintim <rintim@foxmail.com>
2024-07-10 17:24:44 +0800

    pref: use `game.promises.saveConfig`

--- game/entry.js
+++ game/entry.js
@@ -167,8 +167,8 @@ waitUpdate
 
 					let waitings = [];
 
-					waitings.push(new Promise(resolve => game.saveConfig("extensions", lib.config.extensions, void 0, resolve)));
-					waitings.push(new Promise(resolve => game.saveConfig(`extension_${value}_enable`, true, void 0, resolve)));
+					waitings.push(game.promises.saveConfig("extensions", lib.config.extensions));
+					waitings.push(game.promises.saveConfig(`extension_${value}_enable`, true));
 					alert(`扩展${value}已导入成功，点击确定重启游戏`);
 
 					return Promise.allSettled(waitings).then(() => {
