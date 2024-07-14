b1026199f00d21591332580e3494d6ac17501f48
kuangshen04 <2832899707@qq.com>
2024-07-14 16:41:33 +0800

    bugfix

--- noname/game/check.js
+++ noname/game/check.js
@@ -37,8 +37,14 @@ export class Check {
 		items.forEach((item) => {
 			let selectable;
 			if (!lib.filter.cardAiIncluded(item)) selectable = false;
-			else if (useCache && !firstCheck) selectable = cache.includes(item);
-			else selectable = isSelectable(item, event);
+			else if (!useCache) selectable = isSelectable(item, event);
+			else{
+				if(!firstCheck) selectable = cache.includes(item);
+				else{
+					selectable = isSelectable(item, event);
+					if(selectable) cache.push(item);
+				}
+			}
 
 			if (range[1] <= -1) {
 				if (selectable) {
@@ -52,7 +58,6 @@ export class Check {
 			} else {
 				if (selectable && uiSelected.length < range[1]) {
 					item.classList.add("selectable");
-					if (firstCheck) cache.push(item);
 				} else item.classList.remove("selectable");
 			}
 
