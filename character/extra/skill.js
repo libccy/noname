cfea795a228032255269ade9b054e52f815cf469
mengxinzxz <2223529500@qq.com>
2024-07-17 12:03:16 +0800

    删除含chooseToUseAfter全局技的caochuan的单独判定

--- character/extra/skill.js
+++ character/extra/skill.js
@@ -15,7 +15,7 @@ const skills = {
 				//......
 				if (get.type(event.card) != "trick") return false;
 				const history = game.getGlobalHistory("everything", evt => evt.player == player && ["useCard", "respond"].includes(evt.name));
-				return !history.some(evt => Array.isArray(evt.respondTo) && evt.respondTo[2] == event.card && (evt.card.name == "wuxie" || evt.card.name == "caochuan"));
+				return !history.some(evt => Array.isArray(evt.respondTo) && evt.respondTo[2] == event.card && evt.card.name == "wuxie");
 			}
 			const evt = event.getParent(2);
 			if (!evt || evt.name != "useCard") return false;
