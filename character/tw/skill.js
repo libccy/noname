8871acba12d0af78ad28c3970972816b3aabb108
mengxinzxz <2223529500@qq.com>
2024-08-09 12:53:21 +0800

    幻诸葛果

--- character/tw/skill.js
+++ character/tw/skill.js
@@ -747,18 +747,18 @@ const skills = {
 			}
 		},
 	},
-	//幻郭嘉
-	huan_guojia_A: {
+	//幻诸葛果
+	huan_zhugeguo_A: {
 		audio: 2,
 		enable: "phaseUse",
 		onChooseToUse(event) {
-			if (!game.online && event.type == "phase" && !event.huan_guojia_A) {
+			if (!game.online && event.type == "phase" && !event.huan_zhugeguo_A) {
 				const player = event.player;
 				event.set(
-					"huan_guojia_A",
+					"huan_zhugeguo_A",
 					player
 						.getRoundHistory("useSkill", evt => {
-							return evt.skill == "huan_guojia_A";
+							return evt.skill == "huan_zhugeguo_A";
 						})
 						.reduce((list, evt) => {
 							return list.add(evt.targets[0]);
@@ -767,10 +767,10 @@ const skills = {
 			}
 		},
 		filter(event, player) {
-			return player.countCards("he") && game.hasPlayer(target => lib.skill.huan_guojia_A.filterTarget(null, player, target));
+			return player.countCards("he") && game.hasPlayer(target => lib.skill.huan_zhugeguo_A.filterTarget(null, player, target));
 		},
 		filterTarget(card, player, target) {
-			return player != target && !get.event().huan_guojia_A.includes(target);
+			return player != target && !get.event().huan_zhugeguo_A.includes(target);
 		},
 		filterCard: true,
 		selectCard: [1, 2],
@@ -785,8 +785,8 @@ const skills = {
 		prompt: "将至多两张牌标记为“技一”并交给一名本轮未以此法交给其牌的角色",
 		content() {
 			const ID = player.playerid;
-			const skill = "huan_guojia_A_effect",
-				skillID = "huan_guojia_A_" + ID;
+			const skill = "huan_zhugeguo_A_effect",
+				skillID = "huan_zhugeguo_A_" + ID;
 			if (!lib.skill[skillID]) {
 				game.broadcastAll(skillID => {
 					lib.skill[skillID] = { charlotte: true };
@@ -811,16 +811,16 @@ const skills = {
 				},
 			},
 		},
-		group: ["huan_guojia_A_effect", "huan_guojia_A_remove"],
+		group: ["huan_zhugeguo_A_effect", "huan_zhugeguo_A_remove"],
 		subSkill: {
 			effect: {
-				audio: "huan_guojia_A",
+				audio: "huan_zhugeguo_A",
 				trigger: { global: "phaseUseBegin" },
 				filter(event, player) {
-					return event.player.hasCard(card => card.hasGaintag("huan_guojia_A_" + player.playerid), "h");
+					return event.player.hasCard(card => card.hasGaintag("huan_zhugeguo_A_" + player.playerid), "h");
 				},
 				prompt2(event, player) {
-					const num = event.player.storage["huan_guojia_A_effect"][player.playerid];
+					const num = event.player.storage["huan_zhugeguo_A_effect"][player.playerid];
 					return "观看其手牌并将其中至多" + get.cnNumber(num) + "张牌以任意顺序置于牌堆顶";
 				},
 				check(event, player) {
@@ -829,7 +829,7 @@ const skills = {
 				logTarget: "player",
 				async content(event, trigger, player) {
 					const target = trigger.player,
-						num = target.storage["huan_guojia_A_effect"][player.playerid];
+						num = target.storage["huan_zhugeguo_A_effect"][player.playerid];
 					const result = await player
 						.chooseToMove("技一：将" + get.translation(target) + "的至多" + get.cnNumber(num) + "张牌以任意顺序置于牌堆顶", true)
 						.set("list", [[get.translation(target) + "的手牌", target.getCards("h"), "dcsushou_tag"], ["牌堆顶"]])
@@ -859,23 +859,23 @@ const skills = {
 				},
 			},
 			remove: {
-				audio: "huan_guojia_A",
+				audio: "huan_zhugeguo_A",
 				trigger: { player: "phaseBegin" },
 				filter(event, player) {
-					return game.hasPlayer(target => target.hasCard(card => card.hasGaintag("huan_guojia_A_" + player.playerid), "h"));
+					return game.hasPlayer(target => target.hasCard(card => card.hasGaintag("huan_zhugeguo_A_" + player.playerid), "h"));
 				},
 				forced: true,
 				locked: false,
 				async content(event, trigger, player) {
-					const targets = game.filterPlayer(target => target.hasCard(card => card.hasGaintag("huan_guojia_A_" + player.playerid), "h"));
-					const sum = targets.reduce((num, target) => num + target.countCards("h", card => card.hasGaintag("huan_guojia_A_" + player.playerid)), 0);
-					for (const target of targets) target.removeGaintag("huan_guojia_A_" + player.playerid);
+					const targets = game.filterPlayer(target => target.hasCard(card => card.hasGaintag("huan_zhugeguo_A_" + player.playerid), "h"));
+					const sum = targets.reduce((num, target) => num + target.countCards("h", card => card.hasGaintag("huan_zhugeguo_A_" + player.playerid)), 0);
+					for (const target of targets) target.removeGaintag("huan_zhugeguo_A_" + player.playerid);
 					await player.draw(sum);
 				},
 			},
 		},
 	},
-	huan_guojia_B: {
+	huan_zhugeguo_B: {
 		audio: 2,
 		trigger: { target: "useCardToTarget" },
 		filter(event, player) {
