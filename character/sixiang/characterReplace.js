37fbc4e092871fcb19ed55556ed4097b82d9ea98
AstralBarrage <hoshimimaidono@gmail.com>
2024-08-12 22:03:12 +0800

    bugfix,同名切换补充

--- character/sixiang/characterReplace.js
+++ character/sixiang/characterReplace.js
@@ -1,7 +1,7 @@
 const characterReplaces = {
 	sunhao: ["sunhao", "std_sunhao"],
 	lvlingqi: ["lvlingqi", "std_lvlingqi"],
-	chengyu: ["chengyu", "std_chengyu"],
+	chengyu: ["chengyu", "std_chengyu", "dc_sb_chengyu"],
 	fanyufeng: ["fanyufeng", "std_fanyufeng"],
 };
 
