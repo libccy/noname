9764d8da2c7146a40a2c1fdb28dc39aede43fb68
Kobe <160110261+Bryant-F@users.noreply.github.com>
2024-08-16 10:42:19 +0800

    为moveCard的logSkill适配传入数组的情况

--- noname/library/element/content.js
+++ noname/library/element/content.js
@@ -6848,7 +6848,15 @@ export const Content = {
 		"step 1";
 		event.result = result;
 		if (result.bool) {
-			if (event.logSkill) player.logSkill(event.logSkill, result.targets, false);
+			if (event.logSkill) {
+				if (typeof event.logSkill == "string") {
+					player.logSkill(event.logSkill, result.targets, false);
+				} else if (Array.isArray(event.logSkill)) {
+					event.logSkill[1] = result.targets;
+					event.logSkill[2] = false;
+					player.logSkill.apply(player, event.logSkill);
+				}
+			}
 			player.line2(result.targets, "green");
 			event.targets = result.targets;
 		} else {
