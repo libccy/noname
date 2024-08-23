7cf2ba1c839572b721ab457fc7423568e7c0fa3f
lieren2023 <131325076+lieren2023@users.noreply.github.com>
2024-08-23 18:00:36 +0800

    珠联璧合补充re_taishici

--- character/refresh/perfectPairs.js
+++ character/refresh/perfectPairs.js
@@ -1,5 +1,5 @@
 export default {
-	sunben: ["zhouyu", "taishici", "daqiao"],
+	sunben: ["zhouyu", "taishici", "re_taishici", "daqiao"],
 	re_xushu: ["zhaoyun", "sp_zhugeliang"],
 	re_guohuai: ["xiahouyuan", "zhanghe"],
 };
