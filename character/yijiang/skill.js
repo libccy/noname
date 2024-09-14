67806d0eabc21fb2426ddfbb5a5d7b6c195e16a1
Kobe <160110261+Bryant-F@users.noreply.github.com>
2024-09-14 18:01:57 +0800

    简化

--- character/yijiang/skill.js
+++ character/yijiang/skill.js
@@ -9036,79 +9036,6 @@ const skills = {
 				audio: "reqiaoshui",
 				inherit: "qiaoshui3",
 				sourceSkill: "reqiaoshui",
-				content: function () {
-					"step 0";
-					player.removeSkill("reqiaoshui_target");
-					var goon = false;
-					var info = get.info(trigger.card);
-					if (trigger.targets && !info.multitarget) {
-						var players = game.filterPlayer();
-						for (var i = 0; i < players.length; i++) {
-							if (lib.filter.targetEnabled2(trigger.card, player, players[i]) && !trigger.targets.includes(players[i])) {
-								goon = true;
-								break;
-							}
-						}
-					}
-					if (goon) {
-						player
-							.chooseTarget("巧说：是否额外指定一名" + get.translation(trigger.card) + "的目标？", function (card, player, target) {
-								var trigger = _status.event;
-								if (trigger.targets.includes(target)) return false;
-								return lib.filter.targetEnabled2(trigger.card, _status.event.player, target);
-							})
-							.set("ai", function (target) {
-								var trigger = _status.event.getTrigger();
-								var player = _status.event.player;
-								return get.effect(target, trigger.card, player, player);
-							})
-							.set("targets", trigger.targets)
-							.set("card", trigger.card);
-					} else {
-						if (!info.multitarget && trigger.targets && trigger.targets.length > 1) {
-							event.goto(3);
-						}
-					}
-					"step 1";
-					if (result.bool) {
-						if (!event.isMine()) game.delayx();
-						event.target = result.targets[0];
-					} else {
-						event.finish();
-					}
-					"step 2";
-					if (event.target) {
-						player.logSkill("reqiaoshui_target", event.target);
-						trigger.targets.add(event.target);
-					}
-					event.finish();
-					"step 3";
-					player
-						.chooseTarget("巧说：是否减少一名" + get.translation(trigger.card) + "的目标？", function (card, player, target) {
-							return _status.event.targets.includes(target);
-						})
-						.set("ai", function (target) {
-							var trigger = _status.event.getTrigger();
-							return -get.effect(target, trigger.card, trigger.player, _status.event.player);
-						})
-						.set("targets", trigger.targets);
-					"step 4";
-					if (result.bool) {
-						event.targets = result.targets;
-						if (event.isMine()) {
-							player.logSkill("reqiaoshui_target", event.targets);
-							event.finish();
-						}
-						for (var i = 0; i < result.targets.length; i++) {
-							trigger.targets.remove(result.targets[i]);
-						}
-						game.delay();
-					} else {
-						event.finish();
-					}
-					"step 5";
-					player.logSkill("reqiaoshui_target", event.targets);
-				},
 			},
 		},
 		ai: {
@@ -9209,7 +9136,7 @@ const skills = {
 		direct: true,
 		content: function () {
 			"step 0";
-			player.removeSkill("qiaoshui3");
+			player.removeSkill(event.name);
 			var goon = false;
 			var info = get.info(trigger.card);
 			if (trigger.targets && !info.multitarget) {
@@ -9249,7 +9176,7 @@ const skills = {
 			}
 			"step 2";
 			if (event.target) {
-				player.logSkill("qiaoshui3", event.target);
+				player.logSkill(event.name, event.target);
 				trigger.targets.add(event.target);
 			}
 			event.finish();
@@ -9267,7 +9194,7 @@ const skills = {
 			if (result.bool) {
 				event.targets = result.targets;
 				if (event.isMine()) {
-					player.logSkill("qiaoshui3", event.targets);
+					player.logSkill(event.name, event.targets);
 					event.finish();
 				}
 				for (var i = 0; i < result.targets.length; i++) {
@@ -9278,7 +9205,7 @@ const skills = {
 				event.finish();
 			}
 			"step 5";
-			player.logSkill("qiaoshui3", event.targets);
+			player.logSkill(event.name, event.targets);
 		},
 	},
 	qiaoshui4: {
