fa728b1e5dcda8bca69564d77af3aaef867ba6b2
诗笺 <2954700422@qq.com>
2024-10-21 14:18:22 +0800

    添加显示控制台的功能

--- noname/ui/create/menu/pages/otherMenu.js
+++ noname/ui/create/menu/pages/otherMenu.js
@@ -1373,7 +1373,12 @@ export const otherMenu = function (/** @type { boolean | undefined } */ connectM
 								if (Array.isArray(obj)) {
 									return `[${obj.map((v) => parse(v))}]`;
 								} else if (typeof obj == "function") {
-									return `[Function ${obj.name}]`;
+									if (typeof obj.name == "string"){
+										return `[Function ${obj.name}]`;
+									}
+									else {
+										return `[Function]`;
+									}
 								} else if (typeof obj != "string") {
 									if (obj instanceof Error) {
 										return `<span style="color:red;">${String(obj)}</span>`;
@@ -1430,6 +1435,10 @@ export const otherMenu = function (/** @type { boolean | undefined } */ connectM
 			clearButton.listen(() => {
 				pre.innerHTML = "";
 			});
+			if (typeof window.noname_shijianInterfaces?.showDevTools == "function") {
+				game.print("点击以下按钮\n将开启诗笺版内置的控制台");
+				game.print("<button onclick='window.noname_shijianInterfaces.showDevTools();'>开启DevTools</button>");
+			}
 		};
 		if (!get.config("menu_loadondemand")) node._initLink();
 	})();
